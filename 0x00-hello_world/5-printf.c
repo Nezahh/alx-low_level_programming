062cc523e176699cd157f8437e1c4ee328be325f
Nezahh <mbongenigoodman24@gmail.com>
2023-02-16 19:54:23 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio>
+**
+*main - a program that print a line using printf function
+Returns: 0 (success)
+*/
+int (void)
+{   prinf (with proper grammer, but the out was a piece of art,n")
+       Returns(0);
+}	       
